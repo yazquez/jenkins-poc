08f76bcbc145c1d3c5281505fff47fbcdf0709bc
boyle <yazquez@gmail.com>
2015-11-28 23:32:34 +0100

    primer commit

---
+++ jenkins-poc/src/main/java/poc/App.java
@@ -0,0 +1,9 @@
+package poc;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello jenkins");
+    }
+
+}
---
+++ jenkins-poc/src/test/java/poc/AppTest.java
@@ -0,0 +1,12 @@
+package poc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testPoc() {
+        Assert.assertTrue(true);
+    }
+}
